c1ac4705ad1f3ebde7783132b06875a86fc49b7f
MorsCrt <203301130@ogr.selcuk.edu.tr>
2022-04-26 16:13:26 +0300

    aws codebuild path confg

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import src.lib.models.request.controller as ctrl
 
 def main():
-    ctrl.control_list()
-    
+     ctrl.control_list()
+     
 main()
